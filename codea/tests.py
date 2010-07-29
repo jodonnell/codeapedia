c040129765e3cc4472b5f0fdd2623f398eb00925
jodonnell <jodonnell@host2.jacobodonnell2.com>
2010-07-28 23:54:10 -0400

    wrote initial test

--- codea/tests.py
+++ codea/tests.py
@@ -1,23 +1,15 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
+from codea.models import HierarchyDB
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+class FrontPageTest(TestCase):
+    fixtures = ['test.json']
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+    def setUp(self):
+        self.client = Client()
 
->>> 1 + 1 == 2
-True
-"""}
+    def test_front_page(self):
+        response = self.client.get('/')
 
+        self.failUnlessEqual(response.status_code, 200)
+        self.assertContains(response, 'Kent Beck')
